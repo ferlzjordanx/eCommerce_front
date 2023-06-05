98d3a87a1c8e8032601bb5426493dc310b11f883
JorgeFerreira <ferlzjordan@hotmail.com>
2023-06-05 16:56:10 -0400

    added table, format and images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 @NgModule({
